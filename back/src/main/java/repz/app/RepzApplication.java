6a1670a50b595edfd79347f087c367b42db0766a
Zhou <141890618+RobertoZhou@users.noreply.github.com>
2026-03-03 21:01:52 -0300

    Criando o corpo inicial do projeto

---
+++ back/src/main/java/repz/app/RepzApplication.java
@@ -0,0 +1,13 @@
+package repz.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepzApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/repz/app/RepzApplicationTests.java
@@ -0,0 +1,13 @@
+package repz.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
