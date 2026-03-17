6995f19cba7fd9317448dc92ddff44bbcea07b12
joao.correa <joao.correa@wises.com.br>
2026-03-17 20:01:40 -0300

    feat: configure PrimeNG theme and update Angular project settings

--- frontend/repz/src/app/app.config.ts
+++ frontend/repz/src/app/app.config.ts
@@ -3,10 +3,14 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { providePrimeNG } from 'primeng/config';
+import Lara from '@primeng/themes/lara';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    providePrimeNG({ theme: { preset: Lara } })
   ]
 };
