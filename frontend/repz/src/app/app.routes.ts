fc38c945916798cf0d27d9bbd04f127ec4c92028
eduardofabrii <eduardohfabri@gmail.com>
2026-05-16 20:50:41 -0300

    chore(55): remove placeholder do app shell e usa SSR client-side

--- frontend/repz/src/app/app.routes.ts
+++ frontend/repz/src/app/app.routes.ts
@@ -33,10 +33,26 @@ export const routes: Routes = [
     canActivate: [personalGuard]
   },
 
-  { 
-    path: 'aluno', 
+  {
+    path: 'aluno',
     loadComponent: () => import('./features/aluno/aluno').then(m => m.Aluno),
     canActivate: [alunoGuard]
+  },
+
+  {
+    path: 'aluno/ficha-treino',
+    loadComponent: () =>
+      import('./features/aluno/ficha-treino/ficha-treino').then(m => m.FichaTreino),
+    // TODO: reabilitar guard quando o fluxo de login estiver funcionando
+    // canActivate: [alunoGuard]
+  },
+
+  {
+    path: 'personal/aluno/:id/ficha-treino',
+    loadComponent: () =>
+      import('./features/aluno/ficha-treino/ficha-treino').then(m => m.FichaTreino),
+    // TODO: reabilitar guard quando o fluxo de login estiver funcionando
+    // canActivate: [personalGuard]
   },
 
   { path: '**', redirectTo: 'auth' }
